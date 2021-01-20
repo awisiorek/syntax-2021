6f5607bb2c294b998b58e2da4e232d6199daa156
mnschmit <mnschmit@users.noreply.github.com>
2021-01-20 13:02:58 +0100

    Übungsblatt 10 (Getypte Merkmalstrukturen und Unifikation)

---
+++ uebung/exercises_10.py
@@ -0,0 +1,12 @@
+from questions import SingleChoice, OpenQuestion, SelectionList, Lueckentext, MultipleChoice, OpenTextfield
+from questions import single, multiple, offen, selection, luecke
+
+blatt10_1 =[
+    [SingleChoice("Unifizieren f1 und f2?", "Ja", "Nein")],
+    [SingleChoice("Unifizieren f2 und f4?", "Nein", "Ja")]
+]
+
+blatt10_3 =[
+    [SingleChoice("Unifizieren f1 und f4?", "Nein", "Ja")],
+    [SingleChoice("Unifizieren f2 und f3?", "Ja", "Nein")]
+]
